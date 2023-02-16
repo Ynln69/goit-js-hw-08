44b3f01f4096ae160fd2d04b3247133ae87e4986
Yana <yanalunina96@gmail.com>
2023-02-16 15:39:13 +0200

    task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,13 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 const containerGallery = document.querySelector('.gallery');
 const itemsMarkup = createGalleryItems(galleryItems);
 
 containerGallery.insertAdjacentHTML('beforeend', itemsMarkup);
+containerGallery.addEventListener('click', onContainerGalleryClick);
 
 function createGalleryItems(galleryItems) {
   return galleryItems
@@ -24,7 +27,40 @@ function createGalleryItems(galleryItems) {
     })
     .join('');
 }
+
+function onContainerGalleryClick(event) {
+  event.preventDefault();
+  const isItemLink = event.target.classList.contains('gallery__image');
+
+  if (!isItemLink) {
+    return;
+  }
+
+  const onCloseModal = event => {
+    const ESC_KEY = 'Escape';
+
+    if (event.code === ESC_KEY) {
+      instance.close();
+    }
+  };
+
+  const instance = SimpleLightbox.create(
+    `
+      <img src="${event.target.dataset.source}" width="1240" height="800">`,
+    {
+      onShow: instance => {
+        window.addEventListener('keydown', onCloseModal);
+      },
+
+      onClose: instance => {
+        window.removeEventListener('keydown', onCloseModal);
+      },
+    }
+  );
+  instance.show();
+}
+
 const lightbox = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
-  captionsData: 'galleryItems.description',
+  captionsData: 'alt',
 });
