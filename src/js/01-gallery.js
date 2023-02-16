05088a8ab264069aff899e57b0185f5f80580680
Yana <yanalunina96@gmail.com>
2023-02-16 15:05:49 +0200

    підключення бібліотеки + та скрипт з джс

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const containerGallery = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItems(galleryItems);
 
-console.log(galleryItems);
+containerGallery.insertAdjacentHTML('beforeend', itemsMarkup);
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+  </li>
+    `;
+    })
+    .join('');
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'galleryItems.description',
+});
