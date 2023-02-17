3010e1a6c38f64714bcb66275b2e0eb85fcf7ff0
Yana <yanalunina96@gmail.com>
2023-02-17 17:15:09 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const formData = {};
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('input[name=email]'),
+  textarea: document.querySelector('textarea[name=message]'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+populateForm();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateForm() {
+  const savedInputEl = localStorage.getItem(LOCALSTORAGE_KEY);
+
+  if (savedInputEl) {
+    // refs.input.value = savedInputEl;
+    refs.textarea.value = savedInputEl;
+  }
+}
