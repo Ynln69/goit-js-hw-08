1db6b268385e730f2226cd4ec52ed0de5b28a605
Yana <yanalunina96@gmail.com>
2023-02-18 20:38:07 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function currentTime(data) {
+  localStorage.getItem(STORAGE_KEY, data.seconds);
+}
+
+function getDataLS() {
+  const dataFromLS = localStorage.getItem(STORAGE_KEY);
+
+  if (dataFromLS) {
+    return dataFromLS;
+  }
+  return 0;
+}
+player.setCurrentTarget(getDataLS());
+player.on('timeupdate', throttle(currentTime, 1000));
